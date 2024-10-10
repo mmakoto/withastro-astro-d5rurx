c518a6e4d370034b37cd99d246fcdc587b82aadb
mmakoto <makoto@hatsu.com.br>
2024-10-10 17:02:17 -0300

    Module 5 2nd try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig(
-    site: "https://example.com"
+export default defineConfig({
+    site: "https://mellow-stroopwafel-db8a84.netlify.app/"
 });
